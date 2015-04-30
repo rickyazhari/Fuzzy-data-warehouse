8c08c0ce09a4bb4dd7ec3587310f98ec7d814a7c
Ricky Azhari Pratama <coret.team@gmail.com>
2015-04-30 14:36:57 +0700

    UI transisi
    Transisi tombol

--- Program/Spaceman_warehouse/Spaceman_warehouse/MainWindow.xaml.cs
+++ Program/Spaceman_warehouse/Spaceman_warehouse/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace Spaceman_warehouse
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : ModernWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
---
+++ Program/Spaceman_warehouse/Spaceman_warehouse/kon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using FirstFloor.ModernUI.Windows.Controls;
+namespace Spaceman_warehouse
+{
+    class kon
+    {
+        private SqlCommand com = null;
+        private string konf = "server=CORET/SPACEMAN;UID=Coret/kari;PWD=;Database=dw_spaceman";
+        private SqlConnection koneksi = null;
+        
+        private void open_kon()
+        {
+            koneksi = new SqlConnection(konf);
+            koneksi.Open();
+        }
+
+        private void close_kon(){
+           koneksi.Close();
+           koneksi=null;
+        }
+
+       public DataTable  tampil_data(string x)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                
+                open_kon();
+                com = new SqlCommand();
+                com.Connection = koneksi;
+                com.CommandType = CommandType.Text;
+                com.CommandText = x;
+                SqlDataReader mdr = com.ExecuteReader();
+                dt.Load(mdr);
+                close_kon();
+               
+            }
+            catch (SqlException)
+            {
+            
+            }
+            com = null;
+            return dt;
+        }
+    }
+}
