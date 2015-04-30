327696a30547614706213337259305b1b43da2de
Ricky Azhari Pratama <coret.team@gmail.com>
2015-04-30 21:19:34 +0700

    recreate template
    bikin template sendiiri ook

---
+++ Program/Spaceman_warehouse/app_Spaceman_Warehouse/home.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace Spaceman_warehouse
+{
+    /// <summary>
+    /// Interaction logic for home.xaml
+    /// </summary>
+    public partial class home : UserControl
+    {
+       
+        public home()
+        {
+            InitializeComponent();
+        }
+
+        private void DockPanel_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Storyboard m = new Storyboard();
+            m = (Storyboard)TryFindResource("dynamic_bording");
+            m.Begin();
+        }
+
+        private void DockPanel_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Storyboard m = new Storyboard();
+            m = (Storyboard)TryFindResource("dynamic_bording_close");
+            m.Begin();
+        }
+    }
+}
